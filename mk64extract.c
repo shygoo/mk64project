049bf67e9f5ab6806d296e630c04c454519c12c4
shygoo <shyguyhex@gmail.com>
2015-07-27 14:14:00 -0500

    Minor edits

--- mk64extract.c
+++ mk64extract.c
@@ -6,6 +6,29 @@ typedef unsigned short u16;
 typedef unsigned int   u32;
 
 
+char* levelnames[] = {
+	"MARIORACEWAY",     // 00
+	"CHOCOMOUNTAIN",    // 01
+	"BOWSERSCASTLE",    // 02
+	"BANSHEEBOARDWALK", // 03
+	"YOSHIVALLEY",      // 04
+	"FRAPPESNOWLAND",   // 05
+	"KOOPATROOPABEACH", // 06
+	"ROYALRACEWAY",     // 07
+	"LUIGIRACEWAY",     // 08
+	"MOOMOOFARM",       // 09
+	"TOADSTURNPIKE",    // 0A
+	"KALIMARIDESERT",   // 0B
+	"SHERBETLAND",      // 0C
+	"RAINBOWROAD",      // 0D
+	"WARIOSTADIUM",     // 0E
+	"BLOCKFORT",        // 0F
+	"SKYSCRAPER",       // 10
+	"DOUBLEDECK",       // 11
+	"JUNGLEPARKWAY",    // 12
+	"BIGDONUT",         // 13
+};
+
 
 typedef struct {
 	u32 mio0_a_start; // dlists and ?
@@ -27,8 +50,8 @@ int main(int argc, char* argv[]){
 	u32 romsize;
 	void* rom = loadfile(argv[1], &romsize);
 	LevelEntry * entries = rom + 0x122390;
-	char output_filename_a[40];
-	char output_filename_b[40];
+	char output_filename_a[60];
+	char output_filename_b[60];
 	
 	mkdir(argv[2], 0700);
 	
@@ -43,8 +66,8 @@ int main(int argc, char* argv[]){
 		void* decoded_a = mio0decode(mio0_block_a, &decoded_a_size);
 		void* decoded_b = mio0decode(mio0_block_b, &decoded_b_size);
 
-		sprintf(output_filename_a, "%s\\Level_%02d_A.bin\0", argv[2], i);  
-		sprintf(output_filename_b, "%s\\Level_%02d_B.bin\0", argv[2], i);  
+		sprintf(output_filename_a, "%s\\%02d_%s_A.bin\0", argv[2], i, levelnames[i]);  
+		sprintf(output_filename_b, "%s\\%02d_%s_B.bin\0", argv[2], i, levelnames[i]);  
 		
 		writefile(output_filename_a, decoded_a, decoded_a_size);
 		writefile(output_filename_b, decoded_b, decoded_b_size);
