4e6251670d2476ef3845e59fae420ea0e9533bb7
shygoo <shyguyhex@gmail.com>
2015-07-31 16:13:16 -0500

    Add .m64 dumping

--- mk64extract.c
+++ mk64extract.c
@@ -1,10 +1,9 @@
 #include "sg2.h"
 #include "mio0decode.h"
 
-typedef unsigned char  u8;
-typedef unsigned short u16;
-typedef unsigned int   u32;
 
+#define ROM_COURSETABLE 0x122390
+#define ROM_M64BANK     0xBC5F60
 
 char* levelnames[] = {
 	"MARIORACEWAY",     // 00
@@ -31,12 +30,12 @@ char* levelnames[] = {
 
 
 typedef struct {
-	u32 mio0_a_start; // dlists and ?
-	u32 mio0_a_end;
-	u32 mio0_b_start; // ?
-	u32 mio0_b_end;
-	u32 file_unk_a;
-	u32 file_unk_b;
+	u32 mio0_f3d_start; // dlist
+	u32 mio0_f3d_end;
+	u32 mio0_mesh_start; // mesh
+	u32 mio0_mesh_end;
+	u32 unk_start;
+	u32 unk_end;
 	u32 unk1;
 	u32 unk2;
 	u32 unk3;
@@ -46,34 +45,78 @@ typedef struct {
 	u16 padding;
 } LevelEntry;
 
+typedef struct {
+	u32 offset;
+	u32 size;
+} M64Entry;
+
 int main(int argc, char* argv[]){
 	u32 romsize;
-	void* rom = loadfile(argv[1], &romsize);
-	LevelEntry * entries = rom + 0x122390;
-	char output_filename_a[60];
-	char output_filename_b[60];
 	
-	mkdir(argv[2], 0700);
+	char* rom_path         = argv[1];
+	char* output_directory = argv[2];
+	
+	char* rom = loadfile(rom_path, &romsize);
+	LevelEntry*  entries  = &rom[0x122390];
+	M64Entry*  m64entries = &rom[0xBC5F64];
+	u16 m64count = getU16BE(rom, 0xBC5F62);
+	
+	char output_filename_f3d[60];
+	char output_filename_mesh[60];
+	char output_filename_unk[60];
+	
+	mkdir(output_directory, 0700);
+	
+	// dump course graphics:
 	
 	for(int i = 0; i < 20; i++){
-		void* mio0_block_a = rom + swap32(entries[i].mio0_a_start);
-		void* mio0_block_b = rom + swap32(entries[i].mio0_b_start);
+		void* mio0_block_f3d  = &rom[SWAP32(entries[i].mio0_f3d_start];
+		void* mio0_block_mesh = &rom[SWAP32(entries[i].mio0_mesh_start];
+		void* block_unk       = &rom[SWAP32(entries[i].unk_start];
 		
-		u32 decoded_a_size;
-		u32 decoded_b_size;
+		u32 decoded_f3d_size;
+		u32 decoded_mesh_size;
+		u32 raw_unk_size = SWAP32(entries[i].unk_end) - SWAP32(entries[i].unk_start);
 		
-		void* decoded_a = mio0decode(mio0_block_a, &decoded_a_size);
-		void* decoded_b = mio0decode(mio0_block_b, &decoded_b_size);
+		void* decoded_f3d  = mio0decode(mio0_block_f3d, &decoded_f3d_size);
+		void* decoded_mesh = mio0decode(mio0_block_mesh, &decoded_mesh_size);
+		
+		sprintf(output_filename_f3d,  "%s\\%02d_%s_F3D.bin\0",  output_directory, i, levelnames[i]);  
+		sprintf(output_filename_mesh, "%s\\%02d_%s_MESH.bin\0", output_directory, i, levelnames[i]);  
+		sprintf(output_filename_unk,  "%s\\%02d_%s_REFS.bin\0",  output_directory, i, levelnames[i]);  
+		
+		writefile(output_filename_f3d,  decoded_f3d,  decoded_f3d_size);
+		writefile(output_filename_mesh, decoded_mesh, decoded_mesh_size);
+		writefile(output_filename_unk,  block_unk, raw_unk_size);
+		
+		printf(
+			"%02d %-16s\n  F3D  %08X : %08X (mio0)\n  MESH %08X : %08X (mio0)\n  REFS %08X : %08X\n  1 %08X\n  2 %08X\n  3 %08X\n  4 %08X\n  5 %08X\n  6 %04X\n\n",
+			i,
+			levelnames[i],
+			SWAP32(entries[i].mio0_f3d_start),
+			SWAP32(entries[i].mio0_f3d_end),
+			SWAP32(entries[i].mio0_mesh_start),
+			SWAP32(entries[i].mio0_mesh_end),
+			SWAP32(entries[i].unk_start),
+			SWAP32(entries[i].unk_end),
+			SWAP32(entries[i].unk1),
+			SWAP32(entries[i].unk2),
+			SWAP32(entries[i].unk3),
+			SWAP32(entries[i].unk4),
+			SWAP32(entries[i].unk5),
+			SWAP16(entries[i].unk6)
+		);
 
-		sprintf(output_filename_a, "%s\\%02d_%s_A.bin\0", argv[2], i, levelnames[i]);  
-		sprintf(output_filename_b, "%s\\%02d_%s_B.bin\0", argv[2], i, levelnames[i]);  
-		
-		writefile(output_filename_a, decoded_a, decoded_a_size);
-		writefile(output_filename_b, decoded_b, decoded_b_size);
-		
-		printf("Level %02d %-16s %08X (%05X), %08X (%05X)\n", i, levelnames[i], swap32(entries[i].mio0_a_start), decoded_a_size, swap32(entries[i].mio0_b_start), decoded_b_size);
-		
-		free(decoded_a);
-		free(decoded_b);
+		free(decoded_f3d);
+		free(decoded_mesh);
+	}
+	
+	// dump m64 data:
+	
+	char output_filename_m64[60];
+	for(int i = 0; i < m64count; i++){
+		sprintf(output_filename_m64, "%s\\TUNE%02d.m64", output_directory, i);
+		printf("%s\n", output_filename_m64, SWAP32(m64entries[i].offset));
+		writefile(output_filename_m64, &rom[0xBC5F60] + SWAP32(m64entries[i].offset), SWAP32(m64entries[i].size));
 	}
 }
--- sg2.h
+++ sg2.h
@@ -3,85 +3,73 @@
 #include <stdarg.h>
 #include <string.h>
 
-typedef char                 s8;
+
 typedef unsigned char        u8;
-typedef short               s16;
 typedef unsigned short      u16;
-typedef int                 s32;
 typedef unsigned int        u32;
-typedef long long           s64;
 typedef unsigned long long  u64;
-typedef char*               str;
-typedef void*               bin;
 
+typedef char                 s8;
+typedef short               s16;
+typedef int                 s32;
+typedef long long           s64;
 
+#define SWAP16(i) ((((i) & 0xFF) << 8) | (((i) & 0xFF00) >> 8))
+#define SWAP32(i) ((((i) & 0xFF000000) >> 24) | (((i) & 0xFF0000) >> 8) | (((i) & 0xFF00) << 8) | (((i) & 0xFF) << 24))
+#define HI16(i) (((i) >> 16) & 0xFFFF)
+#define LO16(i) ((i) & 0xFFFF)
 
-typedef struct  {
-	char* data;
-	u32   offset;
-	u32   size;
-} buffer;
+#define COUNT(arr) (sizeof(arr) / sizeof(arr[0]))
 
-bin loadfile(str filename, u64* ret_size){ // this is way better than my old one
+// file manipulation
+
+void* loadfile(char* filename, u64* ret_size){
 	FILE* pFile;
 	pFile = fopen(filename, "rb");
 	fseek(pFile, 0, SEEK_END);
 	u64 fsize = ftell(pFile);
 	rewind(pFile);
-	bin buffer = malloc(fsize);
+	void* buffer = malloc(fsize);
 	fread(buffer, (size_t)fsize, 1, pFile);
 	fclose(pFile);
 	*ret_size = fsize;
 	return buffer;
 }
 
-void writefile(str filename, bin buffer, u32 size){
-	FILE * pFile;
+void writefile(char* filename, void* buffer, u64 size){
+	FILE* pFile;
 	pFile = fopen(filename, "wb");
 	fwrite(buffer, (size_t)size, 1, pFile);
 	fclose(pFile);
 }
 
-buffer* new_printbuffer(u32 buffer_size){
-	buffer* p = malloc(sizeof(buffer));
-	p->data = malloc(buffer_size);
-	p->offset = 0;
-	p->size = buffer_size;
-	u32 i;
-	for(i = 0; i < buffer_size; i++){
-		*(p->data + i) = 0x00;
-	}
-	return p;
+// buf manipulation
+
+u32 getU32BE(void* buffer, u32 offset){
+	return SWAP32(*(u32*)(buffer + offset));
 }
 
-void printb(buffer* p, char * format_string, ...){
-	va_list arglist;
-	va_start(arglist, format_string);
-	p->offset += vsprintf(p->data + p->offset, format_string, arglist);
-	va_end(arglist);
+u16 getU16BE(void* buffer, u32 offset){
+	return SWAP16(*(u16*)(buffer + offset));
 }
 
-void buffer_dump(char* path, buffer* b){
-	writefile(path, b->data, b->offset);
+void setU32BE(void* buffer, u32 offset, u32 value){
+	*(u32*)(buffer + offset) = SWAP32(value);
 }
 
-void buffer_fulldump(char* path, buffer* b){
-	writefile(path, b->data, b->size);
+void setU16BE(void* buffer, u32 offset, u16 value){
+	*(u16*)(buffer + offset) = SWAP16(value);
 }
 
-void prompt(char* str, int len){
-	fgets(str,len, stdin);
-	size_t ln = strlen(str) - 1;
-	if(str[ln] == '\n') str[ln] = '\0';
-}
+// debugging
 
-void showdata(void* buffer, int size){
+void viewhex(void* buffer, int size){
 	int i;
 	for(i = 0; i < size; i++){
 		if(i%16 == 0){ //beginning of line
 			printf("%06X %04X | ", buffer+i, i); // print real pointer+offset and current offset
 		}
-		printf("%02X", *(unsigned char*)(buffer+i));
+		printf("%02X", *(u8*)(buffer+i));
 		if((i+1)%4 == 0){ // space every 4 bytes
 			printf(" ");
 		}
